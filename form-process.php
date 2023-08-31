1b0d8765c584eaa39d4f4e975f13ecbeae937059
vlassoveugene <85975072+vlassoveugene@users.noreply.github.com>
2023-08-30 22:43:51 -0500

    83040

--- form-process.php
+++ form-process.php
@@ -42,16 +42,8 @@ if($_POST && isset($_FILES['file']))
         $body = "--$boundary\r\n";
         $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
         $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-        $body .= chunk_split(base64_encode("Name: " . $sender_name . "\r\n" . "RAddress: " . $raddress . "\r\n"  . "Domain: " . $domain . "\r\n")); 
-        $body .= chunk_split(base64_encode("Phone: " . $sender_phone . "\r\n"));
-        $body .= chunk_split(base64_encode("Email Address: " . $sender_email . "\r\n"));
-        $body .= chunk_split(base64_encode("Restaurant Name: " . $rname . "\r\n"));
-        $body .= chunk_split(base64_encode("Domain Name: " . $domain . "\r\n"));
-        $body .= chunk_split(base64_encode("Restaurant Address: " . $raddress . "\r\n"));
-        $body .= chunk_split(base64_encode("Restaurant Phone Number: " . $rphone . "\r\n"));
-        $body .= chunk_split(base64_encode("Restaurant Email Address: " . $remail . "\r\n"));
-        $body .= chunk_split(base64_encode("Message: " . $sender_message . "\r\n"));
-
+        $body .= chunk_split(base64_encode("Name: " . $sender_name . "\r\n" . "Phone: " . $phone . "\r\n" . "Email: " . $sender_email . "\r\n" . "Restaurant Name: " . $rname . "\r\n" . "Domain: " . $domain . "\r\n" . "Restaurant Address: " . $raddress . "\r\n" . "Restaurant Phone: " . $rphone . "\r\n" . "Restaurant Email Address: " . $remail . "\r\n" . "Message: " . $sender_message . "\r\n")); 
+        
       
 
 		//attachments
