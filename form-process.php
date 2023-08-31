d33094791bb6f5c48af82b211ed860e1d512f62e
vlassoveugene <85975072+vlassoveugene@users.noreply.github.com>
2023-08-30 21:28:29 -0500

    83029

--- form-process.php
+++ form-process.php
@@ -1,119 +1,96 @@
 <?php
-if (isset($_POST['email'])) {
-
-    // REPLACE THIS 2 LINES AS YOU DESIRE
-    $email_to = "eugene.vlassov@sparkytech.org";
-    $email_subject = "Get Started Submission";
-
-    function problem($error)
-    {
-        echo "Oh looks like there is some problem with your form data: <br><br>";
-        echo $error . "<br><br>";
-        echo "Please fix those to proceed.<br><br>";
-        die();
+if($_POST && isset($_FILES['file']))
+{
+    header("Location: payment.html");
+	$recipient_email 	= "eugene.vlassov@sparkytech.org"; //recepient
+	$from_email 		= "info@sparkytech.org"; //from email using site domain.
+	$subject			= "Contact Form Submission"; //email subject line
+	
+	$sender_name = filter_var($_POST["s_name"], FILTER_SANITIZE_STRING); //capture sender name
+    $sender_phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING); //capture sender email
+	$sender_email = filter_var($_POST["s_email"], FILTER_SANITIZE_STRING); //capture sender email
+	$sender_message = filter_var($_POST["s_message"], FILTER_SANITIZE_STRING); //capture message
+	$attachments = $_FILES['file'];
+	
+	//php validation
+    if(strlen($sender_name)<4){
+        die('Name is too short or empty');
     }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['fullName']) ||
-        !isset($_POST['phone']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['rname']) ||
-        !isset($_POST['domain']) ||
-        !isset($_POST['raddress']) ||
-        !isset($_POST['rphone']) ||
-        !isset($_POST['remail']) ||
-      
-        !isset($_POST['features']) ||
-        !isset($_POST['message'])
-    ) {
-        problem('Oh looks like there is some problem with your form data.');
+	if (!filter_var($sender_email, FILTER_VALIDATE_EMAIL)) {
+	  die('Invalid email');
+	}
+    if(strlen($sender_message)<4){
+        die('Too short message! Please enter something');
     }
+	
+	$file_count = count($attachments['name']); //count total files attached
+	$boundary = md5("sanwebe.com"); 
+			
+	if($file_count > 0){ //if attachment exists
+		//header
+        $headers = "MIME-Version: 1.0\r\n"; 
+        $headers .= "From:".$from_email."\r\n"; 
+        $headers .= "Reply-To: ".$sender_email."" . "\r\n";
+        $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
+        
+        //message text
+        $body = "--$boundary\r\n";
+        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
+        $body .= chunk_split(base64_encode("Name: " . $sender_name . "\n")); 
+        $body .= chunk_split(base64_encode("Message: " . $phone . "\n"));
+        $body .= chunk_split(base64_encode("Message: " . $sender_message . "\n"));
 
-    $name = $_POST['fullName']; // required
-    $phone = $_POST['phone']; // required
-    $email = $_POST['email']; // required
-    $rname = $_POST['rname']; // required
-    $domain = $_POST['domain']; // required
-    $raddress = $_POST['raddress']; // required
-    $rphone = $_POST['rphone']; // required
-    $remail = $_POST['remail']; // required
- 
-    $features = $_POST['features']; // required
-    $message = $_POST['message']; // required
+        //$email_message .= "Message: " . clean_string($message) . "\n";
 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+		//attachments
+		for ($x = 0; $x < $file_count; $x++){		
+			if(!empty($attachments['name'][$x])){
+				
+				if($attachments['error'][$x]>0) //exit script and output error if we encounter any
+				{
+					$mymsg = array( 
+					1=>"The uploaded file exceeds the upload_max_filesize directive in php.ini", 
+					2=>"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form", 
+					3=>"The uploaded file was only partially uploaded", 
+					4=>"No file was uploaded", 
+					6=>"Missing a temporary folder" ); 
+					die($mymsg[$attachments['error'][$x]]); 
+				}
+				
+				//get file info
+				$file_name = $attachments['name'][$x];
+				$file_size = $attachments['size'][$x];
+				$file_type = $attachments['type'][$x];
+				
+				//read file 
+				$handle = fopen($attachments['tmp_name'][$x], "r");
+				$content = fread($handle, $file_size);
+				fclose($handle);
+				$encoded_content = chunk_split(base64_encode($content)); //split into smaller chunks (RFC 2045)
+				
+				$body .= "--$boundary\r\n";
+				$body .="Content-Type: $file_type; name=\"$file_name\"\r\n";
+				$body .="Content-Disposition: attachment; filename=\"$file_name\"\r\n";
+				$body .="Content-Transfer-Encoding: base64\r\n";
+				$body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
+				$body .= $encoded_content; 
+			}
+		}
 
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'Email address does not seem valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'Name does not seem valid.<br>';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'Message should not be less than 2 characters<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details following:\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $boundary = uniqid();
-
-
-    $file = $_FILES["attachment"]["tmp_name"];
-    $filename = $_FILES["attachment"]["name"];
-    $attachment = chunk_split(base64_encode(file_get_contents($file)));
-
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Phone Number: " . clean_string($phone) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "- - - - - - - - - - - - - - - - - - - - - - - - - - -" . "\n";
-    $email_message .= "Restaurant Name: " . clean_string($rname) . "\n";
-    $email_message .= "Domain: " . clean_string($domain) . "\n";
-    $email_message .= "Restaurant Address: " . clean_string($raddress) . "\n";
-    $email_message .= "Restaurant Phone Number: " . clean_string($rphone) . "\n";
-    $email_message .= "Restaurant Email: " . clean_string($remail) . "\n";
-    $email_message .= "Features: " . clean_string($features) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    $email_message = "--".$boundary."\r\n";
-    $email_message .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    $email_message .= "Content-Transfer-Encoding: base64\r\n\r\n";
-    $email_message .= chunk_split(base64_encode($email_message));
-    $email_message .= "--".$boundary."\r\n";
-    $email_message .= "Content-Type: application/octet-stream; name=\"file.pdf\"\r\n";
-    $email_message .= "Content-Transfer-Encoding: base64\r\n";
-    $email_message .= "Content-Disposition: attachment; filename=\"file.pdf\"\r\n\r\n";
-    $email_message .= $attachment."\r\n";
-    $email_message .= "--".$boundary."--";
-
-    // create email headers
-    $headers = "From: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: multipart/mixed; boundary=\".$boundary.\"\r\n";
-    
-    mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- Replace this as your success message -->
-
-    Thanks for contacting us, we will get back to you as soon as possible.
-
-<?php
+	}else{ //send plain email otherwise
+       $headers = "From:".$from_email."\r\n".
+        "Reply-To: ".$sender_email. "\n" .
+        "X-Mailer: PHP/" . phpversion();
+        $body = $sender_message;
+	}
+		
+	 $sentMail = @mail($recipient_email, $subject, $body, $headers);
+	if($sentMail) //output success or failure messages
+	{       
+		die('Thank you for your email');
+	}else{
+		die('Could not send mail! Please check your PHP mail configuration.');  
+	}
 }
 ?>
